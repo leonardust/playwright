d7f0f9eeb4f64a91fb01bf54147c345e7414b452
Łukasz Komorowski <luk.komorowski@gmail.com>
2023-02-08 14:46:51 +0100

    allure screenshots

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,10 +38,10 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
 
-    // Capture screenshot on failure
-    screenshot: "only-on-failure",
+    trace: 'on-first-retry',
+
+    screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
