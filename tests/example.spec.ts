2482560be705bb8e98bcc9b1ff30c80b1959e17d
lukomo <luk.komorowski@gmail.com>
2023-02-08 11:33:18 +0100

    add npm scripts, changes in example tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,23 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.describe("chromium only", () => {
+  test.skip(({ browserName }) => browserName !== "chromium", "Chromium only!");
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  test("has title @title", async ({ page }) => {
+    await page.goto("https://playwright.dev/");
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playwright/);
+  });
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  test("get started link", async ({ page, browserName }) => {
+    test.skip(browserName === "firefox", "stil working on it");
+    await page.goto("https://playwright.dev/");
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+    // Click the get started link.
+    await page.getByRole("link", { name: "Get started" }).click();
+
+    // Expects the URL to contain intro.
+    await expect(page).toHaveURL(/.*intro/);
+  });
 });
