544e159f8d25a078a003f2272629ab4c131e0e18
lukomo <luk.komorowski@gmail.com>
2023-02-12 22:39:26 +0100

    add service now basic token secret

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
 
     /* Authorization */
     extraHTTPHeaders: {
-      Authorization: "Basic YWRtaW46b1chTEY0LWtNcDF2",
+      Authorization: "${{ secrets.SERVICE_NOW_BASIC_TOKEN }}",
     },
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
